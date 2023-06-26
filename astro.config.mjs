549bc1bdc17c8314a4c5f0501cba83d63994297a
roman.y <roman.yavoriv@didomi.io>
2023-06-26 17:51:54 +0300

    add base links/content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,49 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
-      social: {
-        github: 'https://github.com/withastro/starlight',
+      title: 'Didomi',
+      logo: {
+        src: '/src/assets/logo.svg',
+        replacesTitle: true,
       },
+      customCss: [
+        // Path to your custom CSS file, starting with /
+        '/src/styles/custom.css',
+      ],
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            href: '/src/assets/favicon.svg',
+            rel: 'icon',
+          },
+        },
+      ],
       sidebar: [
         {
-          label: 'Guides',
+          label: 'Getting Started',
+          items: [
+            { label: 'Introduction', link: '/getting-started' },
+          ]
+        },
+        {
+          label: 'SPA',
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            { label: 'History Logs', link: '/spa/history-logs' },
+            { label: 'SSO', link: '/spa/sso' },
           ],
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: 'Utilities',
+          // Instead of manually listing each item, we can use the `autogenerate` option to automatically generate a list of pages.
+          autogenerate: { directory: 'utilities' },
+        },
+        {
+          label: 'Resources',
+          items: [
+            { label: 'Links', link: '/resources' },
+          ],
         },
       ],
     }),
